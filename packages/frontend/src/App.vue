cff0516d8aed1ab38d2e4a54d1213fd650d20d07
Xaiya Schumin <d.schumin@proton.me>
2026-08-03 22:27:05 +0200

    🎉 initialized frontend package
    Signed-off-by: Xaiya Schumin <d.schumin@proton.me>

---
+++ packages/frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+        <p>Running Vue !</p>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-// Here is all born :D
-console.log("Initialization !!");
